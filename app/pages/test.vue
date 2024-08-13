a6d9e20143673d1f9262eb24d8424652b8a851f1
Matt Thompson <matt@echobind.com>
2024-08-13 10:45:02 -0400

    test page (no content)

---
+++ app/pages/test.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+
+// useSeoMeta({
+//   titleTemplate: '',
+//   title: page.value.title,
+//   ogTitle: page.value.title,
+//   description: page.value.description,
+//   ogDescription: page.value.description
+// })
+</script>
+
+<template>
+  <div>
+    <ULandingCard
+      title="Testing Colors"
+      description="Testing Colors"
+      icon="i-heroicons-color-swatch-20-solid"
+      to="/blog"
+      target="_blank"
+      color="blue"
+    />
+  </div>
+</template>
