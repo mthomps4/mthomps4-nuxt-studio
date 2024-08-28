23c9130542bbac471a6625f8cb1bb34824fb5f3b
Matt Thompson <matt@echobind.com>
2024-08-28 11:33:06 -0400

    config for vercel?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -134,5 +134,11 @@ export default defineNuxtConfig({
     }
   },
 
-  compatibilityDate: '2024-07-11'
+  compatibilityDate: '2024-07-11',
+  nitro: {
+    preset: 'vercel'
+  },
+  experimental: {
+    payloadExtraction: false
+  }
 });
